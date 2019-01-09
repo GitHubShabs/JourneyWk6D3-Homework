767a6e9dc02c0e125e86c42bc2e4f97b7bac1ed1
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-09 17:21:12 +0000

    Get Journey End Location passed

---
+++ models/journey.js
@@ -0,0 +1,18 @@
+const Journey = function(startLocation, endLocation, transport, distance) {
+  this.startLocation = startLocation;
+  this.endLocation = endLocation;
+  this.transport = transport;
+  this.distance = distance;
+};
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Journey;
